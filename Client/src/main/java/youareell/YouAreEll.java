eacc1e40a8e9cbaa6fc7fdbc6197e854eb1f1500
kendrang <kendrang@users.noreply.github.com>
2019-12-05 17:21:56 -0500

    got ids

--- Client/src/main/java/youareell/YouAreEll.java
+++ Client/src/main/java/youareell/YouAreEll.java
@@ -1,17 +1,24 @@
 package youareell;
 
 import controllers.*;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
 
 public class YouAreEll {
 
     private MessageController msgCtrl;
     private IdController idCtrl;
+    private String mainUrl = "http://zipcode.rocks:8085"; //initiallized  and assigned to this URL
+    //variable declarations
 
     public YouAreEll (MessageController m, IdController j) {
         // used j because i seems awkward
         this.msgCtrl = m;
         this.idCtrl = j;
     }
+    // setting the class variables equals to the variables being passed in
+    //injecting dependencies
 
     public static void main(String[] args) {
         // hmm: is this Dependency Injection?
@@ -29,6 +36,11 @@ public class YouAreEll {
     }
 
     public String MakeURLCall(String mainurl, String method, String jpayload) {
-        return "nada";
+
+        HttpResponse<JsonNode> jsonResponse
+                = Unirest.get(mainUrl + "/ids")
+                .header("accept", "application/json")
+                .asJson();
+        return jsonResponse.getBody().toString();
     }
 }
